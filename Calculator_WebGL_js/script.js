f7061ba53e2db381999155504374aa679a415026
Ronnoc72 <70102041+Ronnoc72@users.noreply.github.com>
2020-08-27 19:48:19 -0600

    Add files via upload

---
+++ Calculator_WebGL_js/script.js
@@ -0,0 +1,19 @@
+
+var buttons = document.getElementsByName("num_button");
+var calc_window = document.getElementById("calc-window");
+var num1 = document.getElementById("num-1");
+var num2 = document.getElementById("num-2");
+var num3 = document.getElementById("num-3");
+var num4 = document.getElementById("num-4");
+var num5 = document.getElementById("num-5");
+var num6 = document.getElementById("num-6");
+var num7 = document.getElementById("num-7");
+var num8 = document.getElementById("num-8");
+var num9 = document.getElementById("num-9");
+var num_plus = document.getElementById("num-+");
+var num0 = document.getElementById("num-0");
+var num_sub = document.getElementById("num--");
+var num_mult = document.getElementById("num-*");
+var num_divide = document.getElementById("num-/");
+var text = document.createTextNode("6");
+
